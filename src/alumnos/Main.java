1c80149517e218b36751f7327a30c6d7f63a1240
Ricardo García <hazelgrmtz8@gmail.com>
2024-09-11 21:46:35 -0600

    Se agregó un metodo para calcular la edad por medio de la CURP Se agregó para el archivo texto

--- src/alumnos/Main.java
+++ src/alumnos/Main.java
@@ -1,6 +1,9 @@
 package alumnos;
 
 import javax.swing.*;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,6 +24,35 @@ public class Main {
                         System.out.println("Correo"+persona.getCorreo());
                         System.out.println("Discapacidad: "+persona.getDiscapacidad());
                         System.out.println("Edad: "+persona.getEdad());
+
+                        Path path = Path.of("C:\\Arch\\pl.txt");
+                        String Nombre = persona.getNombre();
+                        String Apaterno = persona.getAPaterno();
+                        String AMaterno = persona.getAMaterno();
+                        String Sexo = persona.getSexo();
+                        String Curp = persona.getCURP();
+                        String Correo = persona.getCorreo();
+                        String Telefono = persona.getTelefono();
+                        String Discapacidad = persona.getDiscapacidad();
+                        int Edad = persona.calcularEdad();
+
+
+                        try(FileWriter archivo = new FileWriter(path.toFile(),true)){
+                            PrintWriter pw = new PrintWriter(archivo);
+                            archivo.write(Nombre);
+                            archivo.write("\n"+Apaterno);
+                            archivo.write("\n"+AMaterno);
+                            archivo.write("\n"+Sexo);
+                            archivo.write("\n"+Curp);
+                            archivo.write("\n"+Correo);
+                            archivo.write("\n"+Telefono);
+                            archivo.write("\n"+Discapacidad);
+                            archivo.write("\n"+Edad);
+
+
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, e);
+                        }
 
 
                     }
